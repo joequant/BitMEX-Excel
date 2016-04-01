c8d87a186ae54b9c76810a32d3431f0e27488ac8
Joseph C Wang <joequant@gmail.com>
2016-04-01 23:08:46 +0800

    change to use excelDNA tracing system

--- Logging.cs
+++ Logging.cs
@@ -4,22 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace BitMex
 {
     //quick and dirty logging class
     internal class Logging
     {
-        private static string _logPath;
 
         static Logging()
         {
-            _logPath = System.IO.Path.GetTempPath() + @"\BitMexRTD.log";
         }
 
         internal static void Log(string format, params object[] args)
         {
-            File.AppendAllText(_logPath, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + " - " + string.Format(format, args) + "\r\n");
+            System.Diagnostics.Debug.WriteLine(string.Format(format, args));
         }
 
     }
